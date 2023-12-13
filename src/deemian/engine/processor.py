52c5918a50a6c3d1acc53e66faef261ca77d3579
Muhammad Radifar <m.radifar05@gmail.com>
2023-12-13 13:25:12 +0700

    feat: Add instruction transformer

--- src/deemian/engine/processor.py
+++ src/deemian/engine/processor.py
@@ -1,4 +1,6 @@
-from lark import Lark, Tree
+from collections import namedtuple
+
+from lark import Lark, Tree, Token, Transformer
 
 
 def parser(text: str, grammar: str = "grammar.lark") -> Tree:
@@ -6,3 +8,88 @@ def parser(text: str, grammar: str = "grammar.lark") -> Tree:
     lark_parser = Lark.open(grammar, rel_to=__file__, parser="lalr")
 
     return lark_parser.parse(text)
+
+
+class InstructionTransformer(Transformer):
+    Selection = namedtuple("Selection", "name selection type", defaults=["selection"])
+    BondCorrection = namedtuple("BondCorrection", "name template type", defaults=["bond_correction"])
+    ReadableOutput = namedtuple("ReadableOutput", "results out_file type", defaults=["readable_output"])
+    DeemianData = namedtuple("DeemianData", "outfile type", defaults=["deemian_data"])
+    InteractionList = namedtuple("InteractionList", "interactions type", defaults=["interaction_list"])
+    IncludeIonizable = namedtuple("IncludeIonizable", "charge boolean type", defaults=["include_ionizable"])
+    Conformation = namedtuple("Conformation", "number type", defaults=["conformation"])
+    ConformationRange = namedtuple("ConformationRange", "start end type", defaults=["conformation_range"])
+    InteractingSubject = namedtuple(
+        "InteractingSubject", "subject_1 subject_2 name type", defaults=["interacting_subject"]
+    )
+
+    def selection(self, args):
+        selection_phrase = []
+        for arg in args:
+            if isinstance(arg, Tree):
+                selection_phrase.append(arg.data)
+                selection_phrase.extend([child.value for child in arg.children])
+            elif isinstance(arg, Token):
+                selection_phrase.append(arg.value)
+
+        return tuple(selection_phrase)
+
+    def combine_selection(self, args):
+        selection_phrase = []
+        for arg in args:
+            if isinstance(arg, Tree):
+                selection_phrase.append(arg.data)
+                selection_phrase.extend([child.value for child in arg.children])
+            elif isinstance(arg, Token):
+                selection_phrase.append(arg.value)
+
+        return tuple(selection_phrase)
+
+    def assign_selection(self, args):
+        name = args[0].value
+        selection_chain = []
+        for arg in args[1:]:
+            selection_chain.append(arg)
+
+        return self.Selection(name, selection_chain)
+
+    def bond_correction(self, args):
+        name = args[0].value
+        template = args[1].value
+
+        return self.BondCorrection(name, template)
+
+    def interaction_list(self, args):
+        return self.InteractionList([arg.value for arg in args])
+
+    def include_ionizable(self, args):
+        return self.IncludeIonizable(args[0].value, args[1].value)
+
+    def interacting_subject(self, args):
+        subject_1 = args[1].value
+        subject_2 = args[3].value
+        name = subject_1 + ":" + subject_2
+
+        return self.InteractingSubject(subject_1, subject_2, name)
+
+    def interacting_subject_with_alias(self, args):
+        subject_1 = args[1].value
+        subject_2 = args[3].value
+        name = args[4].value
+
+        return self.InteractingSubject(subject_1, subject_2, name)
+
+    def conformation(self, args):
+        return self.Conformation(args[0].value)
+
+    def conformation_range(self, args):
+        return self.ConformationRange(args[0].value, args[1].value)
+
+    def readable_output(self, args):
+        results = [arg.value for arg in args[:-1]]
+        out_file = args[-1].value
+
+        return self.ReadableOutput(results, out_file)
+
+    def deemian_data(self, args):
+        return self.DeemianData(args[0].value)
--- tests/test_engine_processor.py
+++ tests/test_engine_processor.py
@@ -33,6 +33,27 @@ def vps4_2kw3():
         return processor.parser(text)
 
 
+@pytest.fixture
+def n1_transformed(n1_5nzn):
+    instruction_transformer = processor.InstructionTransformer()
+
+    return instruction_transformer.transform(n1_5nzn)
+
+
+@pytest.fixture
+def spike_transformed(spike_7u0n):
+    instruction_transformer = processor.InstructionTransformer()
+
+    return instruction_transformer.transform(spike_7u0n)
+
+
+@pytest.fixture
+def vps_transformed(vps4_2kw3):
+    instruction_transformer = processor.InstructionTransformer()
+
+    return instruction_transformer.transform(vps4_2kw3)
+
+
 def count_instruction(tree: Tree, data: str):
     instruction_list = list(tree.find_data(data))
 
@@ -63,3 +84,87 @@ def test_instruction_count(n1_5nzn, spike_7u0n, vps4_2kw3):
     assert conformation_range_count == [0, 0, 1]
     assert readable_output_count == [1, 1, 1]
     assert deemian_data_count == [1, 1, 1]
+
+
+def test_molecule_transformer(n1_transformed, spike_transformed):
+    n1_molecule = n1_transformed.children[0]
+    n1_selection_1 = n1_molecule.children[1]
+    n1_selection_2 = n1_molecule.children[2]
+    n1_bond_correction = n1_molecule.children[3]
+
+    spike_molecule = spike_transformed.children[0]
+    spike_selection_1 = spike_molecule.children[1]
+    spike_selection_2 = spike_molecule.children[2]
+    spike_selection_3 = spike_molecule.children[3]
+
+    assert n1_selection_1.name == "protein_A"
+    assert n1_selection_1.selection == [("chain", "A"), ("and", "protein")]
+    assert n1_selection_1.type == "selection"
+    assert n1_selection_2.name == "oseltamivir"
+    assert n1_selection_2.selection == [("chain", "A"), ("and", "resname", "G39")]
+    assert n1_selection_2.type == "selection"
+
+    assert spike_selection_1.name == "ace2_hotspot31"
+    assert spike_selection_1.selection == [("chain", "A"), ("and", "protein"), ("and", "resid_range", "1", "55")]
+    assert spike_selection_1.type == "selection"
+    assert spike_selection_2.name == "ace2_hotspot353"
+    assert spike_selection_2.selection == [("chain", "A"), ("and", "protein"), ("and", "resid_range", "341", "364")]
+    assert spike_selection_2.type == "selection"
+    assert spike_selection_3.name == "spike_rbm"
+    assert spike_selection_3.selection == [("chain", "E"), ("and", "protein"), ("and", "resid_range", "470", "510")]
+    assert spike_selection_3.type == "selection"
+
+    assert n1_bond_correction.name == "oseltamivir"
+    assert n1_bond_correction.template == "CCC(CC)O[C@@H]1C=C(C[C@@H]([C@H]1NC(=O)C)N)C(=O)O"
+    assert n1_bond_correction.type == "bond_correction"
+
+
+def test_measure_transform(n1_transformed, spike_transformed, vps_transformed):
+    n1_measure = n1_transformed.children[1]
+    n1_interactions = n1_measure.children[1]
+    n1_ionizable1 = n1_measure.children[2]
+    n1_ionizable2 = n1_measure.children[3]
+    n1_interacting_subject = n1_measure.children[4]
+    n1_conformation = n1_measure.children[5]
+
+    spike_measure = spike_transformed.children[1]
+    spike_interacting_subject_as = spike_measure.children[4]
+
+    vps_measure = vps_transformed.children[1]
+    vps_conformation_range = vps_measure.children[5]
+
+    assert n1_interactions.interactions == ["all"]
+    assert n1_interactions.type == "interaction_list"
+    assert n1_ionizable1.charge == "positive"
+    assert n1_ionizable1.boolean == "true"
+    assert n1_ionizable1.type == "include_ionizable"
+    assert n1_ionizable2.charge == "negative"
+    assert n1_ionizable2.boolean == "true"
+    assert n1_ionizable2.type == "include_ionizable"
+    assert n1_interacting_subject.subject_1 == "protein_A"
+    assert n1_interacting_subject.subject_2 == "oseltamivir"
+    assert n1_interacting_subject.name == "protein_A:oseltamivir"
+    assert n1_interacting_subject.type == "interacting_subject"
+    assert n1_conformation.number == "1"
+    assert n1_conformation.type == "conformation"
+
+    assert spike_interacting_subject_as.subject_1 == "spike_rbm"
+    assert spike_interacting_subject_as.subject_2 == "ace2_hotspot31"
+    assert spike_interacting_subject_as.name == "rbm_h31"
+    assert spike_interacting_subject_as.type == "interacting_subject"
+
+    assert vps_conformation_range.start == "1"
+    assert vps_conformation_range.end == "20"
+    assert vps_conformation_range.type == "conformation_range"
+
+
+def test_present_transform(n1_transformed):
+    n1_present = n1_transformed.children[2]
+    n1_readable_output = n1_present.children[1]
+    n1_deemian_data = n1_present.children[2]
+
+    assert n1_readable_output.results == ["interactions"]
+    assert n1_readable_output.out_file == "protein_ligand.txt"
+    assert n1_readable_output.type == "readable_output"
+    assert n1_deemian_data.outfile == "protein_ligand.db"
+    assert n1_deemian_data.type == "deemian_data"
